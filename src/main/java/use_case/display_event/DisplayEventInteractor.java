c6560bdb36254eb6d4fe9685c1eeddf6ed1c9221
abudi511 <abdullah.algothami@mail.utoronto.ca>
2025-11-21 02:59:34 +0300

    Update DisplayEventInteractor.java
    fixed the CA

--- src/main/java/use_case/display_event/DisplayEventInteractor.java
+++ src/main/java/use_case/display_event/DisplayEventInteractor.java
@@ -3,6 +3,7 @@ package use_case.display_event;
 import entity.Event;
 
 import java.time.LocalDate;
+import java.util.List;
 
 
 public class DisplayEventInteractor implements DisplayEventInputBoundary{
@@ -13,27 +14,32 @@ public class DisplayEventInteractor implements DisplayEventInputBoundary{
     }
     @Override
     public void execute(DisplayEventInputData input) {
+
         Event event = input.getEvent();
 
-        final LocalDate date = event.getDate();
+        // String formattedDate = date != null ? date.toString() : "TBD";
+        // String priceRange = (priceMin > 0 || priceMax > 0) ?
+                // String.format("Min: %d, Max: %d", priceMin, priceMax) : "N/A";
+        final boolean hasPrice;
         final int priceMin = event.getPriceMin();
         final int priceMax = event.getPriceMax();
-        String formattedDate = date != null ? date.toString() : "TBD";
-        String priceRange = (priceMin > 0 || priceMax > 0) ?
-                String.format("Min: %d, Max: %d", priceMin, priceMax) : "N/A";
-
-        final String eventName = event.getName() + " (id: " + event.getId() +")";
-        final String artist = String.join(", ", event.getArtists());
+        final LocalDate date = event.getDate();
+        final String eventName = event.getName();
+        final List<String> artist = event.getArtists();
         final String venues = event.getVenue();
-        final String location = event.getCity() + ", " + event.getCountry();
-        final String time = formattedDate;
-        final String genre = String.join(", ", event.getGenres());
-        final String price = priceRange;
+        final String city = event.getCity();
+        final String country = event.getCountry();
+        final List<String> genre = event.getGenres();
         final String ticket = event.getTicketUrl();
         final String image = event.getImageURL();
+        hasPrice = priceMin > 0 || priceMax > 0;
 
-        DisplayEventOutputData outputData = new DisplayEventOutputData(eventName, artist, venues, location, time, genre,
-                price, ticket, image);
+
+
+        DisplayEventOutputData outputData = new DisplayEventOutputData(eventName, artist, venues, city, country, date,
+                priceMin, priceMax, ticket,genre, image, hasPrice);
+
+
         presenter.prepareSuccessView(outputData);
 
     }
