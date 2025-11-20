e253f614e348c403f0bc251c38426bf006026035
Hugo Ye <hugoye122@gmail.com>
2025-11-20 18:20:04 -0500

    Removed redundant `LOGIN_ERROR_MESSAGE` constant from `LoginViewModel`.

--- src/main/java/interface_adapter/login/LoginViewModel.java
+++ src/main/java/interface_adapter/login/LoginViewModel.java
@@ -3,7 +3,6 @@ package interface_adapter.login;
 import interface_adapter.ViewModel;
 
 public class LoginViewModel extends ViewModel<LoginState> {
-    public static final String LOGIN_ERROR_MESSAGE = "Username and password do not match. Please try again!";
     public LoginViewModel() {
         super("login");
         setState(new LoginState());
