47938cce52852514bc611c344978ec0eddf933af
Abdullah <abdullah.algothami@mail.utoronto.ca>
2025-11-20 22:08:54 +0300

    changed the present so that it doesnt depend on the state, which would be deleted later same with the view model.

--- src/main/java/interface_adapter/attend_event/AttendEventPresenter.java
+++ src/main/java/interface_adapter/attend_event/AttendEventPresenter.java
@@ -1,14 +1,15 @@
 package interface_adapter.attend_event;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.display_event.DisplayEventViewModel;
 import use_case.attend_event.AttendEventOutputBoundary;
 import use_case.attend_event.AttendEventOutputData;
 
 public class AttendEventPresenter implements AttendEventOutputBoundary {
-    private final AttendEventViewModel attendEventViewModel;
+    private final DisplayEventViewModel attendEventViewModel;
     private final ViewManagerModel viewManagerModel;
 
-    public AttendEventPresenter(AttendEventViewModel attendEventViewModel,
+    public AttendEventPresenter(DisplayEventViewModel attendEventViewModel,
                                 ViewManagerModel viewManagerModel){
         this.attendEventViewModel = attendEventViewModel;
         this.viewManagerModel = viewManagerModel;
@@ -16,23 +17,29 @@ public class AttendEventPresenter implements AttendEventOutputBoundary {
 
     @Override
     public void prepareSuccessView(AttendEventOutputData attendEventOutputData){
-        AttendEventState attendEventState = attendEventViewModel.getState();
+        // DisplayEventViewModel attendEventState = attendEventViewModel.getState();
+        /*
         attendEventState.setUsername(attendEventOutputData.getUsername());
         attendEventState.setEventName(attendEventOutputData.getEventName());
         attendEventState.setMessage(attendEventOutputData.getAdded());
-        attendEventViewModel.setState(attendEventState);
-        attendEventViewModel.firePropertyChanged();
+
+         */
+        // attendEventViewModel.setState(attendEventViewModel);
+        // attendEventViewModel.firePropertyChanged();
         viewManagerModel.setActiveView(attendEventViewModel.getViewName());
         viewManagerModel.firePropertyChanged();
     }
     @Override
     public void prepareFailView(AttendEventOutputData attendEventOutputData){
-        AttendEventState attendEventState = attendEventViewModel.getState();
+        // DisplayEventViewModel attendEventState = attendEventViewModel.getState();
+        /*
         attendEventState.setUsername(attendEventOutputData.getUsername());
         attendEventState.setEventName(attendEventOutputData.getEventName());
         attendEventState.setMessage(attendEventOutputData.getCantAdd());
-        attendEventViewModel.setState(attendEventState);
-        attendEventViewModel.firePropertyChanged();
+
+         */
+        // attendEventViewModel.setState(attendEventState);
+        // attendEventViewModel.firePropertyChanged();
         viewManagerModel.setActiveView(attendEventViewModel.getViewName());
         viewManagerModel.firePropertyChanged();
     }
