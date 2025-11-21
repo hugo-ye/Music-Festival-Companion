fbae09e4d705ac62bb206cf6d2dc245e4c94973b
abudi511 <abdullah.algothami@mail.utoronto.ca>
2025-11-21 03:00:15 +0300

    Update DisplayEventOutputData.java
    fixed CA

--- src/main/java/use_case/display_event/DisplayEventOutputData.java
+++ src/main/java/use_case/display_event/DisplayEventOutputData.java
@@ -2,41 +2,54 @@ package use_case.display_event;
 
 import entity.Event;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class DisplayEventOutputData {
     //private Event event;
     private final String eventName;
-    private final String artists;
+    private final List<String> artists;
     private final String venue;
-    private final String location;
-    private final String date;
-    private final String genres;
-    private final String price;
+    private final String city;
+    private final String country;
+    private final LocalDate date;
+    private final int priceMin;
+    private final int priceMax;
     private final String ticketUrl;
+    private final List<String> genres;
     private final String imageUrl;
+    private boolean hasPrice;
 
-    public DisplayEventOutputData(String eventName, String artists, String venue, String location, String date,
-                                  String genres, String price, String ticketUrl, String imageUrl){
+    public DisplayEventOutputData(String eventName, List<String> artists, String venue, String city,
+                                  String country, LocalDate date, int priceMin, int priceMax, String ticketUrl,
+                                  List<String> genres, String imageUrl, boolean hasPrice ){
         this.eventName = eventName;
         this.artists = artists;
         this.venue = venue;
-        this.location = location;
+        this.city = city;
+        this.country = country;
         this.date = date;
-        this.genres = genres;
-        this.price = price;
+        this.priceMin = priceMin;
+        this.priceMax = priceMax;
         this.ticketUrl = ticketUrl;
+        this.genres = genres;
         this.imageUrl = imageUrl;
+        this.hasPrice = hasPrice;
     }
 
     //getters
     public String getEventName(){return eventName;}
-    public String getArtists(){return artists;}
+    public List<String> getArtists(){return artists;}
     public String getVenue(){return venue;}
-    public String getLocation(){return location;}
-    public String getDate(){return date;}
-    public String getGenres(){return genres;}
-    public String getPrice(){return price;}
+    public String getCity(){return city;}
+    public String getCountry(){return country;}
+    public LocalDate getDate(){return date;}
+    public int getPriceMax() {return priceMax;}
+    public int getPriceMin() {return priceMin;}
+    public List<String> getGenres(){return genres;}
     public String getTicketUrl(){return ticketUrl;}
     public String getImageUrl(){return imageUrl;}
+    public boolean getHasPrice(){return hasPrice;}
 
 
 }
