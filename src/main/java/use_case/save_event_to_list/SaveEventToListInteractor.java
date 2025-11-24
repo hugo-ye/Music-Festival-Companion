2bbb68c348a2ab4c1beef55e3aad4bfef4a236be
Hugo Ye <hugoye122@gmail.com>
2025-11-24 14:38:27 -0500

    fixed logic error in SaveEvent

--- src/main/java/use_case/save_event_to_list/SaveEventToListInteractor.java
+++ src/main/java/use_case/save_event_to_list/SaveEventToListInteractor.java
@@ -3,13 +3,13 @@ package use_case.save_event_to_list;
 import entity.Event;
 import entity.EventList;
 
-public class SaveEventToListInteractor implements SaveEventToListInputBoundary{
+public class SaveEventToListInteractor implements SaveEventToListInputBoundary {
     private final SaveEventToListDataAccessInterface dataAccess;
     private final SaveEventToListOutputBoundary presenter;
 
-    public SaveEventToListInteractor(SaveEventToListDataAccessInterface dataAccess, SaveEventToListOutputBoundary preseneter) {
+    public SaveEventToListInteractor(SaveEventToListDataAccessInterface dataAccess, SaveEventToListOutputBoundary presenter) {
         this.dataAccess = dataAccess;
-        this.presenter = preseneter;
+        this.presenter = presenter;
     }
 
     @Override
@@ -18,16 +18,20 @@ public class SaveEventToListInteractor implements SaveEventToListInputBoundary{
         EventList[] eventLists = inputData.getEventLists();
         StringBuilder message = new StringBuilder();
 
-        // search all eventList, if Event is not in eventList, then add it
-        // since when an Event is added into an arrayList, adding process is omitted if arrayList already contain the
-        // Event
         for (EventList eventList : eventLists) {
-            dataAccess.saveEventToList(event, eventList);
-            message.append("Event added into").append(eventList.getName()).append("\n");
+            if (eventList.getEvents().contains(event)) {
+                message.append("Event is already in ")
+                        .append(eventList.getName())
+                        .append(".\n");
+            } else {
+                dataAccess.saveEventToList(event, eventList);
+                message.append("Event added to ")
+                        .append(eventList.getName())
+                        .append(".\n");
+            }
         }
 
-        SaveEventToListOutputData output= new SaveEventToListOutputData(event, eventLists, message.toString());
-
+        SaveEventToListOutputData output = new SaveEventToListOutputData(event, eventLists, message.toString());
         presenter.present(output);
     }
 }
