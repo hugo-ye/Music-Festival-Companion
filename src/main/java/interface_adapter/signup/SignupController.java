f916953aa753c4ef6e0cc96e74ed8ec0763fe408
Abdullah <abdullah.algothami@mail.utoronto.ca>
2025-11-27 07:20:43 +0300

    Added JavaDoc to the following code: - SignupController - SignUpDataAccessInterface - SignupInteractor - SignupPresenter

--- src/main/java/interface_adapter/signup/SignupController.java
+++ src/main/java/interface_adapter/signup/SignupController.java
@@ -3,6 +3,9 @@ package interface_adapter.signup;
 import use_case.signup.SignupInputBoundary;
 import use_case.signup.SignupInputData;
 
+/**
+ * Controller class for signup use case
+ */
 public class SignupController {
     private final SignupInputBoundary signupInteractor;
 
@@ -10,6 +13,15 @@ public class SignupController {
         this.signupInteractor = signupInteractor;
     }
 
+    /**
+     * Executes the signup process with the provided username and password.
+     * <p>
+     *     This method creates a {@link SignupInputData} object and calls the interactor with it.
+     * </p>
+     * @param username the username entered by the user.
+     * @param password the password entered by the user.
+     * @param repeatPassword the repeated password entered by the user for confirmation.
+     */
     public void execute(String username, String password, String repeatPassword){
         final SignupInputData inputData = new SignupInputData(username, password, repeatPassword);
 
