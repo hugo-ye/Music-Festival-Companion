fe02e6686f8747e5dd7643c044e0e93891b2ed53
Cheuk Fung <cheukfunglau2024@cdnis.edu.hk>
2025-11-30 21:25:26 -0500

    Fixed CheckStyle and added JavaDocs for CreateEventList Data access and input bou

--- src/main/java/use_case/create_event_list/CreateEventListDataAccessInterface.java
+++ src/main/java/use_case/create_event_list/CreateEventListDataAccessInterface.java
@@ -2,10 +2,23 @@ package use_case.create_event_list;
 
 import entity.EventList;
 
+/**
+ * Data access interface for creating event lists.
+ */
 public interface CreateEventListDataAccessInterface {
 
+    /**
+     * Returns whether a list with the given name already exists.
+     *
+     * @param listName the name of the list to check
+     * @return true if a list with this name exists, false otherwise
+     */
     boolean existsByName(String listName);
+
+    /**
+     * Saves the newly created event list.
+     *
+     * @param eventList the event list entity to create
+     */
     void createEventList(EventList eventList);
 }
-
-//existByName and save is done in data access layer for In Memory
--- src/main/java/use_case/create_event_list/CreateEventListInputBoundary.java
+++ src/main/java/use_case/create_event_list/CreateEventListInputBoundary.java
@@ -1,6 +1,11 @@
 package use_case.create_event_list;
 
 public interface CreateEventListInputBoundary {
+
+    /**
+     * Executes the Create Event List use case.
+     *
+     * @param inputData the input data required to create an event list
+     */
     void execute(CreateEventListInputData inputData);
-    // Blueprint for class no code implemented here
 }
