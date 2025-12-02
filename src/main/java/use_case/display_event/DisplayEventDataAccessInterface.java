78262b3e00681ecd05188dbba7d39d69dce2f525
Jeff Liang <jefflyf2006@163.com>
2025-12-02 08:59:13 -0500

    DisplayEvent use case 1. fix all checkstyle error of class regardless name of package 2. unable to address ParameterNumber in line 25 of DisplayEventOutputData, we may need to introduce a builder to deal with it

--- src/main/java/use_case/display_event/DisplayEventDataAccessInterface.java
+++ src/main/java/use_case/display_event/DisplayEventDataAccessInterface.java
@@ -1,9 +1,13 @@
 package use_case.display_event;
 
-import entity.EventList;
-
 import java.util.List;
 
+import entity.EventList;
+
 public interface DisplayEventDataAccessInterface {
+    /**
+     * A method that get all existing eventLists in user.
+     * @return the eventLists of user.
+     */
     List<EventList> getEventLists();
 }
--- src/main/java/use_case/display_event/DisplayEventOutputBoundary.java
+++ src/main/java/use_case/display_event/DisplayEventOutputBoundary.java
@@ -1,5 +1,9 @@
 package use_case.display_event;
 
 public interface DisplayEventOutputBoundary {
+    /**
+     * Prepares the UI when the event is successfully displayed.
+     * @param outputData the event details produced by the interactor.
+     */
     void prepareSuccessView(DisplayEventOutputData outputData);
 }
