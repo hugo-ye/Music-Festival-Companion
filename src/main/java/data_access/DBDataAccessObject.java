9ed626c1971a8cafddc38bc97b6f8111292fd91b
Jeff Liang <jefflyf2006@163.com>
2025-12-02 05:59:11 -0500

    Final modification of checkStyle and Javadoc

--- src/main/java/data_access/DBDataAccessObject.java
+++ src/main/java/data_access/DBDataAccessObject.java
@@ -1,14 +1,12 @@
 package data_access;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import org.json.*;
-import entity.Event;
-import okhttp3.*;
-
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import use_case.search_event.SearchEventDataAccessInterface;
 
 /**
@@ -19,10 +17,12 @@ public class DBDataAccessObject implements SearchEventDataAccessInterface {
     private static final String API_KEY = System.getenv("DB_KEY");
 
     @Override
-    public String search(String keyword, String countryCode, String city, String startDateTime, String endDateTime, String genreIds) {
+    public String search(String keyword, String countryCode, String city, String startDateTime,
+                         String endDateTime, String genreIds) {
+        String result = null;
         // Getting music events from TicketMaster
         final OkHttpClient client = new OkHttpClient().newBuilder().build();
-        HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(BASE_URL)).newBuilder();
+        final HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(BASE_URL)).newBuilder();
         urlBuilder.addQueryParameter("apikey", API_KEY);
         // Get only music events
         urlBuilder.addQueryParameter("classificationName", "music");
@@ -40,15 +40,18 @@ public class DBDataAccessObject implements SearchEventDataAccessInterface {
             final Response response = client.newCall(request).execute();
             if (response.isSuccessful()) {
                 // Checking if body is not null
-                assert response.body() != null;
-                return response.body().string();
-            } else {
+                if (response.body() == null) {
+                    throw new AssertionError();
+                }
+                result = response.body().string();
+            }
+            else {
                 throw new IOException("Something went wrong when fetching" + response);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return null;
-
+        catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return result;
     }
 }
