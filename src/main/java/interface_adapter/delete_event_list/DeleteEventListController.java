16d99d119209e3c09bab0654423dd95d93ee65de
Cheuk Fung <cheukfunglau2024@cdnis.edu.hk>
2025-12-02 16:23:51 -0500

    Fixed Checkstyle and JavaDocs for the interface adaptor for delete_event_list

--- src/main/java/interface_adapter/delete_event_list/DeleteEventListController.java
+++ src/main/java/interface_adapter/delete_event_list/DeleteEventListController.java
@@ -3,15 +3,32 @@ package interface_adapter.delete_event_list;
 import use_case.delete_event_list.DeleteEventListInputBoundary;
 import use_case.delete_event_list.DeleteEventListInputData;
 
+/**
+ * Controller for the delete event list use case.
+ * This class receives the raw list identifier from the UI,
+ * wraps it in a {@link DeleteEventListInputData} object, and passes it
+ * to the {@link DeleteEventListInputBoundary} interactor
+ */
 public class DeleteEventListController {
 
     private final DeleteEventListInputBoundary interactor;
 
+    /**
+     * Constructs a {@code DeleteEventListController} with the given interactor.
+     *
+     * @param interactor the use case interactor to delegate to
+     */
     public DeleteEventListController(DeleteEventListInputBoundary interactor) {
         this.interactor = interactor;
     }
-    public void execute(String listID) {
-        DeleteEventListInputData inputData = new DeleteEventListInputData(listID);
+
+    /**
+     * Executes the delete event list use case for the given list id.
+     *
+     * @param listId the id of the list to delete
+     */
+    public void execute(String listId) {
+        final DeleteEventListInputData inputData = new DeleteEventListInputData(listId);
         interactor.execute(inputData);
     }
 }
--- src/main/java/use_case/delete_event_list/DeleteEventListInputBoundary.java
+++ src/main/java/use_case/delete_event_list/DeleteEventListInputBoundary.java
@@ -1,5 +1,11 @@
 package use_case.delete_event_list;
 
 public interface DeleteEventListInputBoundary {
+
+    /**
+     * Executes the delete event list use case using the provided input data.
+     *
+     * @param inputData the input data object containing the ID of the list to be deleted
+     */
     void execute(DeleteEventListInputData inputData);
 }
